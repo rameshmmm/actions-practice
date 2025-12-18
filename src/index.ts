56c11cdfb297beb1509c5eed2aa429fb7a6e5994
Ramesh Manickam <ramesh.manickam@cdw.com>
2025-12-19 02:55:44 +0530

    sonar fix

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import express from 'express';
+import helmet from "helmet";
 
 const app = express();
+
+app.use(helmet.hidePoweredBy());
 
 app.get('/', (_req, res) => {
   res.json({ message: `Hello from TypeScript app!` });
