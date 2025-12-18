ca43aac2f88f3d909e31c29fac727c9ab6409c90
Ramesh Manickam <ramesh.manickam@cdw.com>
2025-12-19 02:25:42 +0530

    test fix

--- tests/app.test.ts
+++ tests/app.test.ts
@@ -5,6 +5,6 @@ describe('GET /', () => {
   it('returns greeting message', async () => {
     const res = await request(app).get('/');
     expect(res.statusCode).toBe(200);
-    expect(res.body.message).toBe('Hello from TypeScript app!');
+    expect(res.body.message).toBe('Hello from TypeScript app with my-password!');
   });
 });
